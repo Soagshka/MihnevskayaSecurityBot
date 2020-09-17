9e93d06e94363c47e0fe2f7b99d2036ef4097dde
Soagshka <40063763+Soagshka@users.noreply.github.com>
2020-09-17 20:34:50 +0300

    with postgres database v0.4. New cycle starting if there was new deployment or shutdown of service

--- src/main/java/ru/home/security_bot/botapi/handler/FillingRecordHandler.java
+++ src/main/java/ru/home/security_bot/botapi/handler/FillingRecordHandler.java
@@ -67,6 +67,9 @@ public class FillingRecordHandler implements InputMessageHandler {
         long chatId = message.getChatId();
 
         RecordData recordData = userDataCache.getRecordData(userId);
+        if (recordData.getFlatNumber() == 0) {
+            botState = BotState.ASK_FLAT;
+        }
 
         SendMessage sendMessage = null;
 
